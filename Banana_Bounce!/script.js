be5bb926380542555891ed64b969d7f0ca206d54
Realinspirer <realinspirer@outlook.com>
2024-07-09 22:06:29 +0530

    Updated site

---
+++ Banana_Bounce!/script.js
@@ -0,0 +1,12 @@
+"use strict";
+get_json_normal_random("../Scripts/Games_gen.json", "more_games_sec", 3, 0);
+let game_url = "https://html5.gamedistribution.com/427353df07764738958919e5a793f3f0/";
+let game_url_new_tab = "https://html5.gamedistribution.com/427353df07764738958919e5a793f3f0/";
+// window.onload = run_main_frame;
+function run_main_frame() {
+    document.getElementById("play_game_parent").classList.add("hidden");
+    document.getElementById("game_frame").src = game_url;
+}
+function open_in_new_tab() {
+    open(game_url_new_tab);
+}
---
+++ Scripts/game_play_common_script.js
@@ -0,0 +1,43 @@
+"use strict";
+"scroll load".split(" ").forEach(x => window.addEventListener(x, set_scroll_data));
+function set_scroll_data() {
+    document.documentElement.dataset.scroll = window.scrollY.toString();
+}
+let is_full_screen = false;
+window.addEventListener("fullscreenchange", check_for_full_screen);
+function check_for_full_screen(event) {
+    if (!document.fullscreenElement) {
+        if (is_full_screen) {
+            Toggle_full_screen_mode();
+        }
+    }
+}
+function Toggle_full_screen_mode() {
+    is_full_screen = !is_full_screen;
+    document.body.classList.toggle("fullscreen");
+    var text_element = document.getElementById("full_sc_text");
+    toggle_full_screen();
+    if (is_full_screen) {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        });
+        // document.body.scrollTop = document.documentElement.scrollTop = 0;
+        text_element.innerHTML = "Exit full screen mode";
+    }
+    else {
+        text_element.innerHTML = "Full screen mode";
+    }
+}
+function toggle_full_screen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    }
+    else {
+        document.exitFullscreen();
+    }
+}
+function scroll_down() {
+    window.scrollBy({ top: 500, left: 0, behavior: "smooth" });
+}

506cd37a461ed5dae72b64fed92edacb5dd786ae
Realinspirer <realinspirer@outlook.com>
2024-08-01 19:01:11 +0530

    Fixed external url

--- Banana_Bounce!/script.ts
+++ Banana_Bounce!/script.ts
@@ -2,7 +2,7 @@ get_json_normal_random("../Scripts/Games_gen.json", "more_games_sec", 3, 0);
 
 
 let game_url = "https://html5.gamedistribution.com/fb1df24707dd4e8aba2fa7056ec48862/?gd_sdk_referrer_url=https://realinspirer.github.io/Games/Banana_Bounce!/";
-let game_url_new_tab = "https://html5.gamedistribution.com/427353df07764738958919e5a793f3f0/";
+let game_url_new_tab = "https://html5.gamedistribution.com/fb1df24707dd4e8aba2fa7056ec48862/";
 
 // window.onload = run_main_frame;
 

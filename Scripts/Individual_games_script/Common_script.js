1fa7451a71d9a6de24890edd9d7413468339ce8c
Realinspirer <realinspirer@outlook.com>
2024-08-17 17:33:41 +0530

    Updated individual games page

---
+++ Scripts/Individual_games_script/Common_script.js
@@ -0,0 +1,13 @@
+"use strict";
+(function () {
+    let sc_parent = document.getElementById("main_scroller_parent");
+    if (sc_parent == null)
+        return;
+    let sc_main = sc_parent.querySelector(".Features_scroller");
+    let btn_left = sc_parent.querySelector(".sc_button.left");
+    let btn_right = sc_parent.querySelector(".sc_button.right");
+    let scroll_amount = 50 * 0.01 * window.innerWidth;
+    btn_right.addEventListener("click", () => sc_main.scrollBy(scroll_amount, 0));
+    btn_left.addEventListener("click", () => sc_main.scrollBy(-scroll_amount, 0));
+})();
+get_json_normal_random("../Scripts/Games_gen.json", "more_games_sec", 3, 0);

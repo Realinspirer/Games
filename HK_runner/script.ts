9dbf72ef4978238d30e0f5dc5ae69c18109de2db
Realinspirer <realinspirer@outlook.com>
2024-09-18 00:12:52 +0530

    Added another game page

---
+++ HK_runner/script.ts
@@ -0,0 +1,19 @@
+
+(async function(){
+    await get_json_normal_random("/JSON_data/Games_games_list/Games_gen.json", "more_games_sec", 3, "HK Runner!");
+
+    let frame_url = "/Game_builds/HKR/";
+
+    
+    document.getElementById("play_button")?.addEventListener("click", play_clicked);
+    async function play_clicked() {
+        
+        (await play_game_sc).check_size_and_full_screen();
+
+        let game_div = document.querySelector("#play_game_parent");
+        game_div?.classList.add("hidden");
+
+        (<HTMLIFrameElement>document.getElementById("game_frame")).src = frame_url;
+    }
+
+})();

68683fc71ebc01ccd58ab4c44ec8c91bc779d8c6
Realinspirer <realinspirer@outlook.com>
2025-05-15 17:21:07 +0530

    Added another game page

---
+++ MinersFury/Play/script.ts
@@ -0,0 +1,30 @@
+
+(async function(){
+    let current_custom_data = await get_json_normal_random("/JSON_data/Games_games_list/Games_gen.json", "more_games_sec", 3, "Miner's Fury!");
+
+    let frame_url = "/Game_builds/Miners_fury/";
+    
+    // let query = new URLSearchParams(window.location.search);
+    // let req_token = query.get("key");
+
+    // if(!(req_token == "accessid01")){
+        if (current_custom_data?.toLowerCase() != "play") {
+            document.location.replace("../");
+        }
+    // }
+
+
+
+    
+    document.getElementById("play_button")?.addEventListener("click", play_clicked);
+    async function play_clicked() {
+        
+        (await play_game_sc).check_size_and_full_screen();
+
+        let game_div = document.querySelector("#play_game_parent");
+        game_div?.classList.add("hidden");
+
+        (<HTMLIFrameElement>document.getElementById("game_frame")).src = frame_url;
+    }
+
+})();
